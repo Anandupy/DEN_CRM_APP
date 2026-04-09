5506d0d2ec6641702c8ce8abcd8401816bae76f6
Anandupy <94069496+Anandupy@users.noreply.github.com>
2026-04-09 15:54:34 +0530

    Add files via upload

---
+++ src/app/member-management/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppLayout from '@/components/AppLayout';
+import MemberManagementClient from './components/MemberManagementClient';
+
+export default function MemberManagementPage() {
+  return (
+    <AppLayout activePath="/member-management" children={<MemberManagementClient />} />
+  );
+}
